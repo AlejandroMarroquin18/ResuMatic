f8ee55643ed55d42e3ec294b0061e9ad4847ab0d
Alejandro Marroquín <106278424+AlejandroMarroquin18@users.noreply.github.com>
2024-10-26 12:25:18 -0500

    OCR implementado

--- Backend/routes/imageRoutes.js
+++ Backend/routes/imageRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-const { analyzeImageHandler } = require('../controllers/imageController');
+const { analyzeImageHandler, extractTextHandler } = require('../controllers/imageController');
 
 const router = express.Router();
 const upload = multer(); // Para procesar archivos en memoria
 
 // Ruta para analizar imágenes
 router.post('/analyze-image', upload.single('image'), analyzeImageHandler);
+
+// Nueva ruta para extraer texto con OCR
+router.post('/extract-text', upload.single('image'), extractTextHandler);
 
 module.exports = router;
--- Backend/services/azureVisionService.js
+++ Backend/services/azureVisionService.js
@@ -7,6 +7,7 @@ const azureVisionService = {
   subscriptionKey: process.env.AZURE_COMPUTER_VISION_KEY,
 };
 
+//Analiza la imagen
 async function analyzeImage(imageBuffer) {
   try {
     const response = await axios.post(
@@ -17,6 +18,9 @@ async function analyzeImage(imageBuffer) {
           'Ocp-Apim-Subscription-Key': azureVisionService.subscriptionKey,
           'Content-Type': 'application/octet-stream',
         },
+        params: {
+            'language': 'es', // Establecer idioma español
+        },
       }
     );
     return response.data;
@@ -26,4 +30,28 @@ async function analyzeImage(imageBuffer) {
   }
 }
 
-module.exports = { analyzeImage };
+//Extrae el texto de la imagen con OCR
+async function extractTextFromImage(imageBuffer) {
+    try {
+      const response = await axios.post(
+        `${azureVisionService.endpoint}/vision/v3.2/ocr`, // URL para el servicio OCR
+        imageBuffer,
+        {
+          headers: {
+            'Ocp-Apim-Subscription-Key': azureVisionService.subscriptionKey,
+            'Content-Type': 'application/octet-stream',
+          },
+          params: {
+            'language': 'es', // Puedes cambiar esto según el idioma de los textos en las imágenes
+            'detectOrientation': 'true', // Detectar automáticamente la orientación del texto
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error al llamar a la API de Azure OCR:', error.response ? error.response.data : error.message);
+      throw new Error('Error al extraer el texto de la imagen.');
+    }
+  }
+  
+module.exports = { analyzeImage, extractTextFromImage };
